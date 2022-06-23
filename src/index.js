c61bf2ffd1b35dc3be667fc6e63372b78e38807c
+ <+dannaewaltz@gmail.com>
2022-06-23 11:47:22 -0600

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
+const filmsApi = "http://localhost:3000/films";
+
+fetch(filmsApi)
+    .then(response => response.json())
+    .then(data => {renderSingleFilm(data)})
+
+function renderSingleFilm(films) {
+    films.forEach(renderFilms);
+}
+//console.log(films.title)
+
+function renderFilms(films) {
+    // const filmListElement = 
+    //     const filmListItem = document.getElementById("film item");
+    // filmListItem.textContent = film.title;
+    // // // filmListElement.addEventListener("click", () => {
+    // // //     // filmListItem
+    // // //     // filmTitle.innerHTML =
+    // // //     // <img src="${film.poster}" alt="${film.title}"/>
+    // // })
+    const listContainer = document.getElementById("list-container");
+    listContainer.innerText = films.title
+    listContainer.append
+}
 // Your code here
